23c29579f82b19fb84ca163290f06e77bc960b97
med-fitgeek <ndongomedoune.ndiaye@gmail.com>
2024-03-18 07:30:20 +0100

    Completion des classesASTbinary et ASTbinaryOperation

--- src/ast/ASTunaryOperation.java
+++ src/ast/ASTunaryOperation.java
@@ -1,36 +1,50 @@
 package ast;
+
 import java.math.BigInteger;
 
 public class ASTunaryOperation extends ASTexpression {
 
-  public ASTunaryOperation (String operator, ASTexpression operand) {
-    this.operator = operator;
-    this.operand = operand;
-  }
+    private final String operator;
+    private final ASTexpression operand;
 
-  private final String operator;
-  private final ASTexpression operand;
-
-	public ASTexpression getOperand() {
-    return operand;
-  }
-
-	public String getOperator() {
-    return operator;
-  }
-
- public String toString(){
-    return operator + operand.toString();
- }
-
-  public Object eval(){
-    Object op = operand.eval();
-    if (op instanceof BigInteger){
-      BigInteger b = (BigInteger) op;
-      switch (operator){
-      case "-" : return BigInteger.ZERO.subtract(b);
-      };
+    public ASTunaryOperation(String operator, ASTexpression operand) {
+        this.operator = operator;
+        this.operand = operand;
     }
-    return null;
-  }
+
+    public ASTexpression getOperand() {
+        return operand;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    @Override
+    public String toString() {
+        // Retourne une représentation sous forme de chaîne de l'opération unaire
+        return "(" + operator + operand.toString() + ")";
+    }
+
+    @Override
+    public Object eval() {
+        // Évalue l'opérande
+        Object opValue = operand.eval();
+
+        // Assurez-vous que l'opérande est un BigInteger (ou adaptez selon le type attendu)
+        if (!(opValue instanceof BigInteger)) {
+            throw new IllegalArgumentException("Unsupported operand type for unary operation");
+        }
+
+        BigInteger value = (BigInteger) opValue;
+
+        // Applique l'opération unaire en fonction de l'opérateur
+        switch (operator) {
+            case "-":
+                return value.negate();
+            // Ajoutez d'autres cas ici pour d'autres opérations unaires
+            default:
+                throw new UnsupportedOperationException("Unsupported unary operator: " + operator);
+        }
+    }
 }
